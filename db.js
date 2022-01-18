de8d9343de41380b9e011b8cab0c289a56a035e9
Huynie <huychaudesign@gmail.com>
2022-01-17 20:50:45 -0800

    server homepage send response

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 
 const Pool = require("pg").Pool;
 
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "dudeitzme3",
-//   host: "localhost",
-//   port: 5432,
-//   database: "sesh"
-// });
-
 const pool = new Pool({
-  user: "rahuymnn",
-  password: "MRK7ZZxED7LDgbQFB2CYx9M9BrRDMVtn",
-  host: "postgres://rahuymnn:MRK7ZZxED7LDgbQFB2CYx9M9BrRDMVtn@kashin.db.elephantsql.com/rahuymnn",
+  user: "postgres",
+  password: "dudeitzme3",
+  host: "localhost",
   port: 5432,
   database: "sesh"
 });
+
+// const pool = new Pool({
+//   user: "rahuymnn",
+//   password: "MRK7ZZxED7LDgbQFB2CYx9M9BrRDMVtn",
+//   host: "postgres://rahuymnn:MRK7ZZxED7LDgbQFB2CYx9M9BrRDMVtn@kashin.db.elephantsql.com/rahuymnn",
+//   port: 5432,
+//   database: "sesh"
+// });
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const cors = require("cors");
-// const pool = require("./db");
-
+const pool = require("./db");
 
 //middleware
 app.use(cors());
@@ -34,63 +33,73 @@ const connect = async (client) => {
 }
 
 connect(client);
+
+//server homepage
+app.get("/", (req,res) => {
+  try {
+    res.send("Sesh app backend server via NodeJS, Express, and PostgresSQL")
+  } catch (error) {
+    console.log(error);
+  }
+})
+
 //create spot
-// app.post("/spots", async (req, res) => {
-//   try {
-//     const { address } = req.body;
-//     const newAddress = await pool.query("INSERT INTO practiceSpots (address) VALUES($1) RETURNING *", [address]);
-//     console.log(res.json(newAddress.rows[0]));
+app.post("/spots", async (req, res) => {
+  try {
+    const { address } = req.body;
+    const newAddress = await pool.query("INSERT INTO practiceSpots (address) VALUES($1) RETURNING *", [address]);
+    res.json(newAddress.rows[0]);
     
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// })
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 
 //get all spots
-// app.get("/spots", async (req, res) => {
-//   try {
-//     const allSpots = await pool.query("SELECT * FROM practiceSpots");
-//     res.json(allSpots.rows);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// })
+app.get("/spots", async (req, res) => {
+  try {
+    const allSpots = await pool.query("SELECT * FROM practiceSpots");
+    res.json(allSpots.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 //get 1 spot
-// app.get("/spots/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const spot = await pool.query("SELECT * FROM practicespots WHERE spot_id = $1", [id]);
-//     res.json(spot.rows[0]);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// })
+app.get("/spots/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const spot = await pool.query("SELECT * FROM practicespots WHERE spot_id = $1", [id]);
+    res.json(spot.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 
 //update spot
-// app.put("/spots/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { address } = req.body;
-//     const updateAddress = await pool.query("UPDATE practiceSpots SET address = $1 WHERE spot_id = $2", [address, id]);
-//     res.json("address updated");
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// })
+app.put("/spots/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { address } = req.body;
+    const updateAddress = await pool.query("UPDATE practiceSpots SET address = $1 WHERE spot_id = $2", [address, id]);
+    res.json("address updated");
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 //delete spot
-// app.delete("/spots/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const deleteAddress = await pool.query("DELETE FROM practiceSpots WHERE spot_id = $1", [id]);
-//     res.json("address deleted");
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// })
+app.delete("/spots/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteAddress = await pool.query("DELETE FROM practiceSpots WHERE spot_id = $1", [id]);
+    res.json("address deleted");
+  } catch (error) {
+    console.log(error.message);
+  }
+})
 
 const PORT = process.env.PORT || 4001
 app.listen(PORT, () => {
